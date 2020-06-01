6415b425dcb3c353058b03ccb03cf2f00b09499b
Teng Wei Song <weisong.teng.work@gmail.com>
2020-06-01 20:46:43 +0800

    obtain access token from auth0 management

--- CoffeeShopTalk.WebApi/Controllers/UserProfileController.cs
+++ CoffeeShopTalk.WebApi/Controllers/UserProfileController.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using CoffeeShopTalk.WebApi.Models;
 using CoffeeShopTalk.WebApi.Models.Requests;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace CoffeeShopTalk.WebApi.Controllers
 {
@@ -13,9 +16,11 @@ namespace CoffeeShopTalk.WebApi.Controllers
     public class UserProfileController : ControllerBase
     {
         private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _configuration;
 
-        public UserProfileController(IHttpClientFactory clientFactory)
+        public UserProfileController(IHttpClientFactory clientFactory, IConfiguration configuration)
         {
+            _configuration = configuration;
             _clientFactory = clientFactory;
         }
 
@@ -26,7 +31,8 @@ namespace CoffeeShopTalk.WebApi.Controllers
             if (userIdFromAccessToken != request.UserId)
                 return BadRequest("Invalid user");
 
-            var accessToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImR1aEY0WDRnRnJnSklrSUdJaThWRyJ9.eyJpc3MiOiJodHRwczovL2NvZmZlZS1zaG9wLXRhbGstZGV2LmF1dGgwLmNvbS8iLCJzdWIiOiJMenJzT3U1RUtqTE1JNklCcEt4Z2laRjc3dndJT1JGakBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9jb2ZmZWUtc2hvcC10YWxrLWRldi5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTU5MDkxNTQzOCwiZXhwIjoxNTkxMDAxODM4LCJhenAiOiJMenJzT3U1RUtqTE1JNklCcEt4Z2laRjc3dndJT1JGaiIsInNjb3BlIjoicmVhZDpjbGllbnRfZ3JhbnRzIGNyZWF0ZTpjbGllbnRfZ3JhbnRzIGRlbGV0ZTpjbGllbnRfZ3JhbnRzIHVwZGF0ZTpjbGllbnRfZ3JhbnRzIHJlYWQ6dXNlcnMgdXBkYXRlOnVzZXJzIGRlbGV0ZTp1c2VycyBjcmVhdGU6dXNlcnMgcmVhZDp1c2Vyc19hcHBfbWV0YWRhdGEgdXBkYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSBkZWxldGU6dXNlcnNfYXBwX21ldGFkYXRhIGNyZWF0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgcmVhZDp1c2VyX2N1c3RvbV9ibG9ja3MgY3JlYXRlOnVzZXJfY3VzdG9tX2Jsb2NrcyBkZWxldGU6dXNlcl9jdXN0b21fYmxvY2tzIGNyZWF0ZTp1c2VyX3RpY2tldHMgcmVhZDpjbGllbnRzIHVwZGF0ZTpjbGllbnRzIGRlbGV0ZTpjbGllbnRzIGNyZWF0ZTpjbGllbnRzIHJlYWQ6Y2xpZW50X2tleXMgdXBkYXRlOmNsaWVudF9rZXlzIGRlbGV0ZTpjbGllbnRfa2V5cyBjcmVhdGU6Y2xpZW50X2tleXMgcmVhZDpjb25uZWN0aW9ucyB1cGRhdGU6Y29ubmVjdGlvbnMgZGVsZXRlOmNvbm5lY3Rpb25zIGNyZWF0ZTpjb25uZWN0aW9ucyByZWFkOnJlc291cmNlX3NlcnZlcnMgdXBkYXRlOnJlc291cmNlX3NlcnZlcnMgZGVsZXRlOnJlc291cmNlX3NlcnZlcnMgY3JlYXRlOnJlc291cmNlX3NlcnZlcnMgcmVhZDpkZXZpY2VfY3JlZGVudGlhbHMgdXBkYXRlOmRldmljZV9jcmVkZW50aWFscyBkZWxldGU6ZGV2aWNlX2NyZWRlbnRpYWxzIGNyZWF0ZTpkZXZpY2VfY3JlZGVudGlhbHMgcmVhZDpydWxlcyB1cGRhdGU6cnVsZXMgZGVsZXRlOnJ1bGVzIGNyZWF0ZTpydWxlcyByZWFkOnJ1bGVzX2NvbmZpZ3MgdXBkYXRlOnJ1bGVzX2NvbmZpZ3MgZGVsZXRlOnJ1bGVzX2NvbmZpZ3MgcmVhZDpob29rcyB1cGRhdGU6aG9va3MgZGVsZXRlOmhvb2tzIGNyZWF0ZTpob29rcyByZWFkOmFjdGlvbnMgdXBkYXRlOmFjdGlvbnMgZGVsZXRlOmFjdGlvbnMgY3JlYXRlOmFjdGlvbnMgcmVhZDplbWFpbF9wcm92aWRlciB1cGRhdGU6ZW1haWxfcHJvdmlkZXIgZGVsZXRlOmVtYWlsX3Byb3ZpZGVyIGNyZWF0ZTplbWFpbF9wcm92aWRlciBibGFja2xpc3Q6dG9rZW5zIHJlYWQ6c3RhdHMgcmVhZDp0ZW5hbnRfc2V0dGluZ3MgdXBkYXRlOnRlbmFudF9zZXR0aW5ncyByZWFkOmxvZ3MgcmVhZDpzaGllbGRzIGNyZWF0ZTpzaGllbGRzIHVwZGF0ZTpzaGllbGRzIGRlbGV0ZTpzaGllbGRzIHJlYWQ6YW5vbWFseV9ibG9ja3MgZGVsZXRlOmFub21hbHlfYmxvY2tzIHVwZGF0ZTp0cmlnZ2VycyByZWFkOnRyaWdnZXJzIHJlYWQ6Z3JhbnRzIGRlbGV0ZTpncmFudHMgcmVhZDpndWFyZGlhbl9mYWN0b3JzIHVwZGF0ZTpndWFyZGlhbl9mYWN0b3JzIHJlYWQ6Z3VhcmRpYW5fZW5yb2xsbWVudHMgZGVsZXRlOmd1YXJkaWFuX2Vucm9sbG1lbnRzIGNyZWF0ZTpndWFyZGlhbl9lbnJvbGxtZW50X3RpY2tldHMgcmVhZDp1c2VyX2lkcF90b2tlbnMgY3JlYXRlOnBhc3N3b3Jkc19jaGVja2luZ19qb2IgZGVsZXRlOnBhc3N3b3Jkc19jaGVja2luZ19qb2IgcmVhZDpjdXN0b21fZG9tYWlucyBkZWxldGU6Y3VzdG9tX2RvbWFpbnMgY3JlYXRlOmN1c3RvbV9kb21haW5zIHVwZGF0ZTpjdXN0b21fZG9tYWlucyByZWFkOmVtYWlsX3RlbXBsYXRlcyBjcmVhdGU6ZW1haWxfdGVtcGxhdGVzIHVwZGF0ZTplbWFpbF90ZW1wbGF0ZXMgcmVhZDptZmFfcG9saWNpZXMgdXBkYXRlOm1mYV9wb2xpY2llcyByZWFkOnJvbGVzIGNyZWF0ZTpyb2xlcyBkZWxldGU6cm9sZXMgdXBkYXRlOnJvbGVzIHJlYWQ6cHJvbXB0cyB1cGRhdGU6cHJvbXB0cyByZWFkOmJyYW5kaW5nIHVwZGF0ZTpicmFuZGluZyBkZWxldGU6YnJhbmRpbmcgcmVhZDpsb2dfc3RyZWFtcyBjcmVhdGU6bG9nX3N0cmVhbXMgZGVsZXRlOmxvZ19zdHJlYW1zIHVwZGF0ZTpsb2dfc3RyZWFtcyBjcmVhdGU6c2lnbmluZ19rZXlzIHJlYWQ6c2lnbmluZ19rZXlzIHVwZGF0ZTpzaWduaW5nX2tleXMgcmVhZDpsaW1pdHMgdXBkYXRlOmxpbWl0cyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.tAyBZq8x-N9qiuXDRb4GZJzAb6-Ezr--J0-pY5A-iEGoM1xQGt9ksajZq8qlVsQ56wwaorO0sjLz47G-5jd6K5CWEb1jKs5vRp_xJ6wPpNKrjJHN1AKNb6LxlNIFY8xAkkpfIFTQptrz8MqUrxmyMjTx_UgOR6lgZ2EZkia8ZxyjFszzV9BF587y0miE0k7GCLWiQ1gyD7tmf2iAlORCRZ_ltB3YlW8dayn5JiduMookj9Tov7yVNvTfWfWuAjdTP5rCNWR_BI9Z2y-cEMy50FVfzj0X4R6pnGEaRAcumEDVb4o410X48SWGbWEcCWA9rWJ2AQWq5C0h2s589bAJlQ";
+            var accessToken = await GetAccessToken();
+
             var client = _clientFactory.CreateClient("Auth0 Management API");
             client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
             var userProfileUpdateRequest = new Auth0UserProfileUpdateRequest()
@@ -39,6 +45,24 @@ namespace CoffeeShopTalk.WebApi.Controllers
             response.EnsureSuccessStatusCode();
 
             return Ok(await response.Content.ReadAsStringAsync());
+        }
+
+        private async Task<string> GetAccessToken()
+        {
+            var client = _clientFactory.CreateClient();
+
+            var nvc = new Dictionary<string, string>();
+            nvc.Add("grant_type", "client_credentials");
+            nvc.Add("client_id", _configuration.GetValue<string>("Auth0:ClientId"));
+            nvc.Add("client_secret", _configuration.GetValue<string>("Auth0:ClientSecret"));
+            nvc.Add("audience", _configuration.GetValue<string>("Auth0:Audience"));
+
+            var request = new FormUrlEncodedContent(nvc);
+            var response = await client.PostAsync("https://coffee-shop-talk-dev.auth0.com/oauth/token", request);
+
+            var data = await response.Content.ReadAsStringAsync();
+            var data2 = JsonSerializer.Deserialize<Auth0ManagementApiAccessToken>(data);
+            return data2.AccessToken;
         }
     }
 }
